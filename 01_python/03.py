b66fd5d6230a4de3a9d82eb896b6321d22a36a02
Monugya | curious_coder <132900296+monugya-07@users.noreply.github.com>
2025-10-25 19:57:41 +0000

    Commit

---
+++ 01_python/03.py
@@ -0,0 +1,22 @@
+print("Hello, type your name : ")
+n = input()
+print("Hello," , n, "How are you ?")
+
+m = input("Enter your name : ")
+print(m)
+
+l = input("What is your location ?")
+print(l)
+
+name = "Monugya"
+name = "Borchetia"
+name = "Monugya"    #Overwrite
+
+age = 40
+age = 30
+age = age+1
+print(age)
+
+r = int(input("Enter the radius of the circle: "))
+area = 3.14 * r * r
+print("The area of the circle : ", area)
