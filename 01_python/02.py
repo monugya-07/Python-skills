26d0b4347aa2824ac6070ce610d6222f39ca66ad
Monugya | curious_coder <132900296+monugya-07@users.noreply.github.com>
2025-10-25 19:38:19 +0000

    commit

---
+++ 01_python/02.py
@@ -0,0 +1,28 @@
+a = 10
+print(a)
+b = 20
+print(b)
+print(a+b)
+print(a*b)
+
+a = a+1
+print(a)
+
+a = a+1
+print(a)
+
+a = a+1
+print(a)
+
+a = a+1
+print(a)
+
+n = int(input())
+print("Enter a number:",n)
+print(n)
+
+print(n+1)
+print(n+2)
+print(n+3)
+print(n+4)
+print(n+5)
